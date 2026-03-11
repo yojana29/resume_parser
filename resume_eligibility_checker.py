70887f091f1267120f6a061204b56d6a643803f1
yojana29 <yojanapandey29@gmail.com>
2026-03-11 21:36:37 +0545

    feature 1 added

---
+++ resume_eligibility_checker.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+experience = int(input("Enter years of experience: "))
+if experience >= 5:
+    print(name,"is eligible for Senior developer role")
+else:
+    print(name,"is eligible for Junior Developer role")
