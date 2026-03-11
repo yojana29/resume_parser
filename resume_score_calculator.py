6851922afefb162a965fa5f0cc219fc11f642868
yojana29 <yojanapandey29@gmail.com>
2026-03-11 22:00:28 +0545

    feature 5 added

--- resume_score_calculator.py
+++ resume_score_calculator.py
@@ -1,10 +1,13 @@
 score = 0
 python = input("Do you know Python? (yess/no):")
 aws = input("Do you know AWS? (yes/no): ")
+react = input("Do you know React? (yes/no): ")
 if python == "yes":
     score = score + 10
 
 if aws == "yes":
     score = score + 10
-
+if react == "yes":
+    score = score + 10
+    
 print("Resume Score:",score)   
