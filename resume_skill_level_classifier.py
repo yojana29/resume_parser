f2948872f3bf4729467fdc4661866a20ef739091
yojana29 <yojanapandey29@gmail.com>
2026-03-11 21:41:52 +0545

    feature 2 added

---
+++ resume_skill_level_classifier.py
@@ -0,0 +1,7 @@
+skills = int(input("How many programming skills do you know? "))
+if skills >= 8:
+    print("Strong Developer Profile ")
+elif skills >= 3:
+    print("Average Developer Profile ")
+else :
+    print("Beginner Developer")
