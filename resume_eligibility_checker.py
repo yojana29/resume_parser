bcb391440c1519e0e03507fa317c58803c85e401
YojanaPandey <167634762+yojana29@users.noreply.github.com>
2026-03-12 07:26:38 +0545

    Update resume_eligibility_checker.py

--- resume_eligibility_checker.py
+++ resume_eligibility_checker.py
@@ -1,6 +1,8 @@
-name = input("Enter your name: ")
-experience = int(input("Enter years of experience: "))
-if experience >= 5:
-    print(name,"is eligible for Senior developer role")
-else:
-    print(name,"is eligible for Junior Developer role")
+def resumeEligibility():
+    name = input("Enter your name: ")
+    experience = int(input("Enter years of experience: "))
+    if experience >= 5:
+        print(name,"is eligible for Senior developer role")
+    else:
+        print(name,"is eligible for Junior Developer role")
+ resumeEligibility()       
